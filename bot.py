dabac88f3eb1ba404d2fe2dfb0f0ada03ae30b83
GitKodland <125306926+GitKodland@users.noreply.github.com>
2025-02-01 20:39:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,41 +4,41 @@ import discord
 from discord.ext import commands
 from config import TOKEN
 
-# Initializing the database manager
+# Veri tabanı yöneticisini başlatma
 manager = DB_Map("database.db")
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print("Bot started")
+    print("Bot başlatıldı!")
 
 @bot.command()
 async def start(ctx: commands.Context):
-    await ctx.send(f"Hi, {ctx.author.name}. Enter !help_me to explore the list of available commands")
+    await ctx.send(f"Merhaba, {ctx.author.name}. Mevcut komutların listesini keşfetmek için !help_me yazın.")
 
 @bot.command()
 async def help_me(ctx: commands.Context):
     await ctx.send(
-        # Implement the command that will display the list of available commands
+        # Kullanılabilir komutların listesini gösterecek olan komutu yazın.
     )
 
 @bot.command()
 async def show_city(ctx: commands.Context, *, city_name=""):
-    # Implement the command that will display the map with the specified city
+    # Belirtilen şehirle birlikte haritayı gösterecek komutu yazın.
 
 @bot.command()
 async def show_my_cities(ctx: commands.Context):
-    cities = manager.select_cities(ctx.author.id)  # Getting the list of cities remembered by the user
+    cities = manager.select_cities(ctx.author.id)  # Kullanıcının kaydettiği şehirlerin listesini alma
 
-    # Implement the command that will display the map with the user's cities
+    # Kullanıcının şehirleriyle birlikte haritayı gösterecek komutu yazın
 
 @bot.command()
 async def remember_city(ctx: commands.Context, *, city_name=""):
-    if manager.add_city(ctx.author.id, city_name):  # Checking if the city exists in the database; if so, adding it to user's memory
-        await ctx.send(f'The city of {city_name} has been saved successfully!')
+    if manager.add_city(ctx.author.id, city_name):  # Şehir adının format uygunluğunu kontrol etme. Başarılıysa şehri kaydet!
+        await ctx.send(f'{city_name} şehri başarıyla kaydedildi!')
     else:
-        await ctx.send("Incorrect format. Please enter the name of the city in English, with a space after the command.")
+        await ctx.send("Hatalı format. Lütfen şehir adını İngilizce olarak ve komuttan sonra bir boşluk bırakarak girin.")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
